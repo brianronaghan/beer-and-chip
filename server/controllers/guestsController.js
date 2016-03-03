ef121278d1e243c97112c5512bc53a3fadb33015
Brian Ronaghan <brian.ronaghan@gmail.com>
2016-03-02 20:48:27 -0800

    guestsCTRL

--- server/controllers/guestsController.js
+++ server/controllers/guestsController.js
@@ -1,11 +1,12 @@
 var GuestQuery = require('../queries/guestQueries');
+var ItemQuery = require('../queries/itemQueries');
 
 module.exports = {
   get: function(req, res) {
     var eventID = req.body.eventID;
     GuestQuery.getAll(eventID, function(guests) {
       res.json(guests);
-    })
+    });
   },
 
   post: function(req, res) {
@@ -20,13 +21,26 @@ module.exports = {
     var newAttrs = req.body;
     GuestQuery.updateOne(guestID, newAttrs, function() {
       res.send();
-    })
+    });
   },
 
   delete: function(req, res) {
-    var guestID = req.params.guestID; 
-    GuestQuery.deleteOne(guestID, function(){
-      res.send();
+    var guestID = req.params.guestID;
+    var eventID = req.params.eventID;
+    var unAssId;
+    GuestQuery.findStillNeeded(eventID, function (id){
+      unAssId = id;
+      // need to add function where (BEFORE WE DELETE THE GUEST)
+      // we reassign any items a deleted guest had claimed to the STILL NEEDED 'guest'
+      ItemQuery.reassignUsersItems(guestID, eventID, unAssId, function(){
+        console.log(req.params.guestID);
+        GuestQuery.deleteOne(req.params.guestID, function(){
+          res.send();
+        });
+      });
     });
+
+
+
   }
-}
+};
