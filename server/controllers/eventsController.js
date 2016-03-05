3bd1da0bab31176ef0db99f661a30aa1d16810eb
Brian Ronaghan <brian.ronaghan@gmail.com>
2016-03-05 14:28:52 -0800

    test

--- server/controllers/eventsController.js
+++ server/controllers/eventsController.js
@@ -29,7 +29,8 @@ module.exports = {
           // ADD THE STILL NEEDED ID to these parameters:
           var snID = GuestQuery.findStillNeeded(event.id, function (snID) {
             ItemQuery.addAll(event.id, data.items, snID,function() {
-              var url = "http://162.243.218.81:3000/#/eventdetails/" + event.id;
+              var url = "http://www.google.com";
+              // var url = "http://162.243.218.81:3000/#/eventdetails/" + event.id;
               // call send email
               EmailQuery.sendInvite('many', 'none',event.id, url, function (){
                 res.send();
